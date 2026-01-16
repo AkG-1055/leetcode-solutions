c71fbbfc3276e1e95d53a8365f17ae3280737e8a
AkG-1055 <goel.akshat1005@gmail.com>
2026-01-16 16:34:12 +0530

    Time: 1428 ms (5.65%), Space: 28.1 MB (62.25%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int max_count = 0;
+        int i, j;
+
+        for (i = 0; i < n; i++){
+            int count = 1;
+            for(j = i + 1; j < n; j++){
+                if (nums[i] == nums[j]){
+                    count = count + 1;
+                }
+                if (count > max_count){
+                    max_count = count;
+                }
+            }
+            if (max_count > n/2){
+                return nums[i];
+            }
+        }    
+        return nums[0];
+    }
+};
