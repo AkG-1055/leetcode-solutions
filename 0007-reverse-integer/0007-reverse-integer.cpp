d5fdbbad0ebe1081b855bb4e34c47cc4b3a4dc14
AkG-1055 <goel.akshat1005@gmail.com>
2026-03-29 20:35:57 +0530

    Time: 5 ms (20.05%), Space: 8.6 MB (19.97%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int digit = 0;
+        int rev = 0;
+        while (x != 0) {
+            digit = x % 10;
+
+            // BOUNDARY CHECKS:
+            //+VE SIDE -
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7))
+                return 0;
+
+            // -VE SIDE -
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))
+                return 0;
+            
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+        return rev;
+    }
+};
