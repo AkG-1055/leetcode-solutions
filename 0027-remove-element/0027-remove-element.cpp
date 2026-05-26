491f10a8bc21eedef15d4259dbc8a9178e317005
AkG-1055 <goel.akshat1005@gmail.com>
2026-05-26 18:01:18 +0530

    Time: 0 ms (100%), Space: 11.7 MB (83.28%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++){
+            if (nums[i] == val){
+                nums.erase(remove(nums.begin(), nums.end(), val), nums.end());
+            }
+        }
+
+        int k = nums.size();
+
+        return k;
+    }
+};
