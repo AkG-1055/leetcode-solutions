71a5d424fd9b00d5eabb651daf931dd400c1c8a6
AkG-1055 <goel.akshat1005@gmail.com>
2026-06-02 13:31:14 +0530

    Time: 7 ms (74.38%), Space: 32.1 MB (56.86%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int res = 0;
+
+        for (int i = 0; i < n; i++){
+            res = res + nums[i];
+            i++;
+        }
+        return res;
+    }
+};
