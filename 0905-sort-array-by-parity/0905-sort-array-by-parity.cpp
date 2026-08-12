7537acdd1d991cd7a31486355b220203b15c3536
AkG-1055 <goel.akshat1005@gmail.com>
2026-08-12 16:40:50 +0530

    Time: 1 ms (18.48%), Space: 20.7 MB (22.16%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % 2 == 0) {
+                nums[i] = nums[i] * -1;
+            }
+        }
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % 2 == 0) {
+                nums[i] = nums[i] * -1;
+            }
+            else{
+                break;
+            }
+        }
+
+        return nums;
+    }
+};
