417a7412ee4401b70cdc5041806f6c28613de44e
AkG-1055 <goel.akshat1005@gmail.com>
2026-09-13 17:10:23 +0530

    Time: 22 ms (50.68%), Space: 87.3 MB (83.56%) - LeetHub

---
+++ 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int totalSteps(vector<int>& nums) {
+        stack<pair<int, int>> st;
+        int ans = 0;
+
+        for (int x : nums) {
+            int steps = 0;
+
+            while (!st.empty() && x >= st.top().first) {
+                steps = max(steps, st.top().second);
+                st.pop();
+            }
+
+            if (!st.empty()) {
+                steps++;
+            }
+
+            ans = max(ans, steps);
+            st.push({x, steps});
+        }
+
+        return ans;
+    }
+};
